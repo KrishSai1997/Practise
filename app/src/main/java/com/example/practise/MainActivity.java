6ddd206f06b65d50567049fc70b8b1a7d33daa52
KrishSai1997 <saichikku1997@gmail.com>
2019-03-20 01:30:19 +0530

    Firebase in implementation

--- app/src/main/java/com/example/practise/MainActivity.java
+++ app/src/main/java/com/example/practise/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.practise;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,5 +28,29 @@ public class MainActivity extends AppCompatActivity {
         Password = (EditText)findViewById(R.id.etPassword);
         Login = (Button)findViewById(R.id.btnLogin);
         Signup = (TextView)findViewById(R.id.tvRegister);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName == "Admin") && (userPassword == "admin123")){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(MainActivity.this,"Invalid Details",Toast.LENGTH_SHORT).show();
+        }
     }
 }
