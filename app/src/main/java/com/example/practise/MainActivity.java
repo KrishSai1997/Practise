2e819c0dcc29dca2f98ea49f115ee741f8208f85
KrishSai1997 <saichikku1997@gmail.com>
2019-04-22 02:42:54 +0530

    Again From The Scratch 20/4-22/4

--- app/src/main/java/com/example/practise/MainActivity.java
+++ app/src/main/java/com/example/practise/MainActivity.java
@@ -14,12 +14,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.practise.Model.Users;
+import com.example.practise.Prevalent.Prevalent;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private Button Login;
     private TextView Signup;
-    private FirebaseAuth firebaseAuth;
+    public FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+    //private DataSnapshot dataSnapshot;
 
 
     @Override
@@ -37,18 +45,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Name = (EditText)findViewById(R.id.etName);
-        Password = (EditText)findViewById(R.id.etPassword);
-        Login = (Button)findViewById(R.id.btnLogin);
-        Signup = (TextView)findViewById(R.id.tvRegister);
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+        Signup = (TextView) findViewById(R.id.tvRegister);
         FirebaseApp.initializeApp(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
-        progressDialog =new ProgressDialog((this));
+        progressDialog = new ProgressDialog((this));
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        if (user != null){
+        if (user != null) {
             finish();
             startActivity(new Intent(MainActivity.this, HomeActivity.class));
         }
@@ -67,9 +75,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        /*FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseAuth.AuthStateListener authListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    String userId = firebaseUser.getUid();
+                    String userEmail = firebaseUser.getEmail();
+                }
+            }
+
+
+        };*/
+
     }
 
-    private void validate(String userName, String userPassword){
+
+
+    private void validate(final String userName, String userPassword){
+
+        //final DatabaseReference RootRef;
+        //RootRef = FirebaseDatabase.getInstance().getReference();
 
         progressDialog.setMessage("Loading... Please Wait");
         progressDialog.show();
@@ -79,7 +107,9 @@ public class MainActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                  if (task.isSuccessful()){
                      progressDialog.dismiss();
+                     finish();
                      startActivity(new Intent(MainActivity.this, HomeActivity.class));
+
                      Toast.makeText(MainActivity.this, "Login Successfull",Toast.LENGTH_SHORT).show();
                  }else{
                      Toast.makeText(MainActivity.this, task.getException().getMessage(),Toast.LENGTH_LONG).show();
