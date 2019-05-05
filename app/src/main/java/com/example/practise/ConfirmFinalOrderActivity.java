0f5adede870c48b361df26a1023ae986d0b6e542
KrishSai1997 <saichikku1997@gmail.com>
2019-05-05 22:43:57 +0530

    Again From The Scratch 20/4-22/4

--- app/src/main/java/com/example/practise/ConfirmFinalOrderActivity.java
+++ app/src/main/java/com/example/practise/ConfirmFinalOrderActivity.java
@@ -1,9 +1,24 @@
 package com.example.practise;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.practise.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ConfirmFinalOrderActivity extends AppCompatActivity {
 
@@ -24,5 +39,86 @@ public class ConfirmFinalOrderActivity extends AppCompatActivity {
         phoneEditText = (EditText) findViewById(R.id.shipment_phone_number);
         addressEditText = (EditText) findViewById(R.id.shipment_address);
         cityEditText = (EditText) findViewById(R.id.shipment_city);
+
+        confirmOrderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Check();
+            }
+        });
+    }
+
+    private void Check() {
+        if(TextUtils.isEmpty(nameEditText.getText().toString()))
+        {
+            Toast.makeText(this, "Please Provide Name.", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(phoneEditText.getText().toString()))
+        {
+            Toast.makeText(this, "Please Provide Phone Number.", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(addressEditText.getText().toString()))
+        {
+            Toast.makeText(this, "Please Provide Address.", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(TextUtils.isEmpty(cityEditText.getText().toString()))
+        {
+            Toast.makeText(this, "Please Provide City.", Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+            ConfirmOrder();
+        }
+    }
+
+    private void ConfirmOrder() {
+        final String saveCurrentDate, saveCurrentTime;
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat CurrentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(calForDate.getTime());
+
+        final DatabaseReference ordersRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+
+        HashMap<String, Object> ordersMap = new HashMap<>();
+        //ordersMap.put("totalAmount", totalAmount);
+        ordersMap.put("pname", nameEditText.getText().toString());
+        ordersMap.put("phone", phoneEditText.getText().toString());
+        ordersMap.put("address", addressEditText.getText().toString());
+        ordersMap.put("city", cityEditText.getText().toString());
+        ordersMap.put("date", saveCurrentDate);
+        ordersMap.put("time", saveCurrentTime);
+        ordersMap.put("state","Not Shipped");
+
+        ordersRef.updateChildren(ordersMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    FirebaseDatabase.getInstance().getReference()
+                            .child("Cart List")
+                            .child("User View")
+                            .removeValue()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(ConfirmFinalOrderActivity.this, "Order Placed Successfully", Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(ConfirmFinalOrderActivity.this, HomeActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 }
